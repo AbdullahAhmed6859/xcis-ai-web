e15058220331b4b1bc94e5644ae9a5e32731923d
AbdullahAhmed6859 <aa09303@st.habib.edu.pk>
2026-01-10 18:33:14 +0500

    customer reviews bg colour fixed

--- src/features/page-builder/blocks/customer-reviews.tsx
+++ src/features/page-builder/blocks/customer-reviews.tsx
@@ -1,14 +1,21 @@
+import { SectionDescription } from "@/features/layout/section-description";
+import { SectionHeader } from "@/features/layout/section-header";
+import { SectionHeading } from "@/features/layout/section-heading";
 import Container from "../../layout/Container";
 import { ReviewsSectionProps } from "./page-builder-types";
 
-export function CustomerReviews({ heading, text }: ReviewsSectionProps) {
+export function CustomerReviews({
+  heading,
+  text,
+  backgroundColor,
+}: ReviewsSectionProps) {
   return (
     <Container>
       <div className="flex flex-col gap-4 items-center">
-        <div className="text-dark-blue flex flex-col gap-2 items-center">
-          <h2 className="text-4xl font-semibold">{heading}</h2>
-          <p className="text-lg text-center max-w-3xl">{text}</p>
-        </div>
+        <SectionHeader backgroundColor={backgroundColor}>
+          <SectionHeading>{heading}</SectionHeading>
+          <SectionDescription>{text}</SectionDescription>
+        </SectionHeader>
         <div className="text-lg text-center max-w-3xl">
           <div>Customer Reviews Here</div>
         </div>
