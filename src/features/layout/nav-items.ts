c4a2d3608ee820a017517d3d97d0c1a825e774b4
AbdullahAhmed6859 <aa09303@st.habib.edu.pk>
2026-01-26 00:52:33 +0500

    typescript error fixed

--- src/features/layout/nav-items.ts
+++ src/features/layout/nav-items.ts
@@ -2,29 +2,29 @@ export const NAV_ITEMS = [
   { label: "Home", href: "/" },
   { label: "Services", href: "/services" },
   { label: "Case Studies", href: "/case-studies" },
-  { label: "Training & Talent", href: "/training-talent" },
-  { label: "Careers", href: "/careers" },
-  { label: "About", href: "/about" },
+  // { label: "Training & Talent", href: "/training-talent" },
+  // { label: "Careers", href: "/careers" },
+  // { label: "About", href: "/about" },
   { label: "Media", href: "/media" },
-  { label: "Contact", href: "/contact" },
+  // { label: "Contact", href: "/contact" },
 ];
 
 export const FOOTER_COMPANY_ITEMS = [
   { label: "Services", href: "/services" },
   { label: "Case Studies", href: "/case-studies" },
-  { label: "Training & Talent", href: "/training-talent" },
-  { label: "Careers", href: "/careers" },
+  // { label: "Training & Talent", href: "/training-talent" },
+  // { label: "Careers", href: "/careers" },
 ];
 
 export const FOOTER_HELP_ITEMS = [
-  { label: "About", href: "/about" },
+  // { label: "About", href: "/about" },
   { label: "Media", href: "/media" },
-  { label: "Locations", href: "/locations" },
-  { label: "Contact", href: "/contact" },
+  { label: "Locations", href: "/" },
+  // { label: "Contact", href: "/contact" },
 ];
 
 export const FOOTER_ITEMS = [
-  { label: "Services", href: "/services" },
+  // { label: "Services", href: "/services" },
   { label: "About", href: "/about" },
   { label: "Case Studies", href: "/case-studies" },
   { label: "Media", href: "/media" },
