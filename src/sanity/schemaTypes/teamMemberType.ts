3cdd375e04e066bb7c403d250a052405d387c05a
AbdullahAhmed6859 <aa09303@st.habib.edu.pk>
2026-01-27 10:23:42 +0500

    team is not compulsory

--- src/sanity/schemaTypes/teamMemberType.ts
+++ src/sanity/schemaTypes/teamMemberType.ts
@@ -28,7 +28,6 @@ export const teamMemberType = defineType({
       name: "teams",
       type: "array",
       of: [defineArrayMember({ type: "reference", to: { type: "team" } })],
-      validation: (rule) => rule.required(),
     }),
   ],
   preview: {
